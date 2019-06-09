7133040d7204e52550652d114018a7222e8efd4b
MingYao <xuauul@gmail.com>
2019-06-09 13:50:44 +0800

    orderList style

--- frontend/src/components/OrderList.jsx
+++ frontend/src/components/OrderList.jsx
@@ -5,7 +5,7 @@ import {
     Link
 } from 'react-router-dom';
 
-import {Col, Row, Button, Container, Tab, Nav, Card, Image} from 'react-bootstrap';
+import {Col, Row, Button, Container, Tab, Nav, Card, Image, Table} from 'react-bootstrap';
 import './OrderList.css';
 
 export default class OrderList extends React.Component{
@@ -34,7 +34,7 @@ export default class OrderList extends React.Component{
               title: 'Andy',
               talent:'piano',
               price:'5000',
-              payment:'已付款',
+              payment:'未付款',
               img: `images/piano.jpg`,
               updated: '2019/05/13',
               score:9.0
@@ -46,29 +46,66 @@ export default class OrderList extends React.Component{
 
 
       let cards = data.map(d => {
+        let title_style = {
+            background: "#17a2b8",
+            color: "white",
+            display: "inline-block",
+            padding: "3px 5px",
+            letterSpacing: "3px",
+            borderRadius: "5px"
+        }
+        var state = '';
+        if (d.payment === '已付款') {
+            state = '已付款';
+        } else {
+            state = <button className="m-2 pay-btn">付款</button>;
+        }
         return (
-            <Card  key={d.key} className="m-4" style={{width: "100%"}}>
-                <div className="d-flex flex-row align-items-center">
-                    <Card.Img  className="round float-left m-3" style={{width: "15vh", height: "15vh", border: "3px solid #f4f4f4", borderRadius: "50%"}} src={d.img}/>
-                    <Card.Body>
-                        <Card.Title style={{ background: "#17a2b8", color: "white", display: "inline-block", padding: "0 5px", letterSpacing: "3px"}}>{d.title}</Card.Title>
-                        <Card.Text>才藝：{d.talent}</Card.Text>
-                        <Card.Text>價錢：{d.price}</Card.Text>
-                        <Card.Text>評分：{d.score}</Card.Text>
-                    </Card.Body>                    
-                </div>
-                <Card.Footer className="d-flex flex-row align-items-center justify-content-between">
-                    <Card.Text className="m-0">付款狀態：{d.payment}</Card.Text>
-                    <button className="m-2 pay-btn">付款</button>
-                </Card.Footer>
-            </Card>
+            <tr>
+                <td style={{verticalAlign: "middle"}}>{d.key}</td>
+                <td style={{verticalAlign: "middle"}}>{d.talent}</td>
+                <td style={{verticalAlign: "middle"}} className="d-none d-lg-table-cell d-xl-none"><img className="round float-left m-3" style={{ width: "15vh", height: "15vh", border: "3px solid #f4f4f4", borderRadius: "50%" }} src={d.img} /></td>
+                <td style={{verticalAlign: "middle"}}>{d.title}</td>
+                <td style={{verticalAlign: "middle"}}>{d.score}</td>
+                <td style={{verticalAlign: "middle"}}>{d.price}</td>
+                <td style={{verticalAlign: "middle"}}>{state}</td>
+            </tr>
+            // <Card  key={d.key} className="m-4" style={{width: "100%"}}>
+            //     <div className="d-flex flex-row align-items-center">
+            //         <Card.Img  className="round float-left m-3" style={{width: "15vh", height: "15vh", border: "3px solid #f4f4f4", borderRadius: "50%"}} src={d.img}/>
+            //         <Card.Body>
+            //             <Card.Title style={title_style}>{d.title}</Card.Title>
+            //             <Card.Text>才藝：{d.talent}</Card.Text>
+            //             <Card.Text>價錢：{d.price}</Card.Text>
+            //             <Card.Text>評分：{d.score}</Card.Text>
+            //         </Card.Body>                    
+            //     </div>
+            //     <Card.Footer className="d-flex flex-row align-items-center justify-content-between">
+            //         <Card.Text className="m-0">付款狀態：{d.payment}</Card.Text>
+            //         <button className="m-2 pay-btn">付款</button>
+            //     </Card.Footer>
+            // </Card>
         )
 
       });
       return(
-        <Container>
-            {cards}
-        </Container>
+        <Table responsive bordered hover>
+            <thead>
+                <th style={{verticalAlign: "middle"}}>編號</th>
+                <th style={{verticalAlign: "middle"}}>才藝</th>
+                <th style={{verticalAlign: "middle"}} className="d-none d-lg-table-cell d-xl-none">圖片</th>
+                <th style={{verticalAlign: "middle"}}>姓名</th>
+                <th style={{verticalAlign: "middle"}}>評分</th>
+                <th style={{verticalAlign: "middle"}}>價錢</th>
+                <th style={{verticalAlign: "middle"}}>操作</th>
+            </thead>
+            <tbody>
+                {cards}
+            </tbody>
+        </Table>
+        // <Container>
+        //     {cards}
+        // </Container>
       )
 
     };
